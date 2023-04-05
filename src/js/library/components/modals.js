fc8ff407d4db55f0c6fd19006b964883fcb1e42f
uladzimir <vova108vova@gmail.com>
2023-04-05 21:00:00 +0300

    interactive create modal windows for pages, start

--- src/js/library/components/modals.js
+++ src/js/library/components/modals.js
@@ -25,3 +25,38 @@ $.prototype.modal = function () {
     });
 };
 $('[data-toggle="mod"]').modal();
+
+$.prototype.createModal = function ({ texts, buttons } = {}) {
+    for (let i = 0; i < this.length; i++) {
+        const div = document.createElement('div');
+        div.classList.add('modal');
+        div.setAttribute('id', this[i].getAttribute('data-target').slice(1));
+        const btns = [];
+        //buttons = {
+        //count: num,
+        //settings: [[text, classNames[], close, callback]]}
+        for (let j = 0; j < buttons.count; j++) {
+            const btn = document.createElement('button');
+            btn.classList.add('btn', ...buttons.settings[j][1]);
+        }
+        div.innerHTML =
+            `
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <button class="close" data-close>&times;</button>
+                        <div class="modal-header">
+                            <div class="modal-title">${texts.title}</div>
+                        </div>
+                        <div class="modal-body">
+                            ${texts.body}
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn orange-btn" data-close>Close</button>
+                            <button class="btn blue-btn">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+    }
+};
